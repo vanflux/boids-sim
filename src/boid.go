541a3e4fb20f03ebb7ae99d1008d40e3c2f0fb24
vanflux <lucasabruzzi@hotmail.com>
2025-05-23 19:01:46 -0300

    feat: img drawing

--- src/boid.go
+++ src/boid.go
@@ -3,7 +3,6 @@ package boids
 import (
 	"image"
 	"image/color"
-	"math"
 	"math/rand/v2"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -43,26 +42,16 @@ func (b *Boid) Draw(screen *ebiten.Image) {
 	var path vector.Path
 
 	// E
-	path.MoveTo(20, 20)
-	path.LineTo(20, 70)
-	path.LineTo(70, 70)
-	path.LineTo(70, 60)
-	path.LineTo(30, 60)
-	path.LineTo(30, 50)
-	path.LineTo(70, 50)
-	path.LineTo(70, 40)
-	path.LineTo(30, 40)
-	path.LineTo(30, 30)
-	path.LineTo(70, 30)
-	path.LineTo(70, 20)
-	path.Close()
+	path.MoveTo(0, 0)
+	path.LineTo(50, 25)
+	path.LineTo(0, 50)
+	path.LineTo(0, 0)
 
 	vertices := []ebiten.Vertex{}
 	indices := []uint16{}
 
 	vertices, indices = path.AppendVerticesAndIndicesForStroke(vertices[:0], indices[:0], &vector.StrokeOptions{
-		Width:    5,
-		LineJoin: vector.LineJoinRound,
+		Width: 1,
 	})
 
 	for i := range vertices {
@@ -74,14 +63,15 @@ func (b *Boid) Draw(screen *ebiten.Image) {
 		vertices[i].ColorG = 0x56 / float32(0xff)
 		vertices[i].ColorB = 0x20 / float32(0xff)
 		vertices[i].ColorA = 1
+		// print("vertices[i]:", i, "    ", vertices[i].DstX, ", ", vertices[i].DstY, ", ", vertices[i].SrcX, ", ", vertices[i].SrcY, ", ", vertices[i].ColorR, ", ", vertices[i].ColorG, ", ", vertices[i].ColorB, ", ", vertices[i].ColorA, ", ", vertices[i].DstX, ", ", vertices[i].DstX, "    \n\n")
 	}
 
 	objImg := ebiten.NewImage(80, 80)
 	objImg.DrawTriangles(vertices, indices, whiteSubImage, &ebiten.DrawTrianglesOptions{AntiAlias: true, FillRule: ebiten.FillRuleEvenOdd})
 
 	op := ebiten.DrawImageOptions{}
-	op.GeoM.Rotate(math.Pi / 180 * 45)
-	op.GeoM.Translate(100, 100)
+	// op.GeoM.Rotate(math.Pi / 180 * 45)
+	// op.GeoM.Translate(100, 100)
 	screen.DrawImage(objImg, &op)
 
 	// vector.StrokeLine(screen, b.x, b.y, b.x+b.size, b.y+b.size/2, 1, color.RGBA{R: 255, G: 0, B: 0, A: 255}, true)
